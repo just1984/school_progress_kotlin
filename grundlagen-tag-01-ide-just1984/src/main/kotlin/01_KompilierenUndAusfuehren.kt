42e8158d50f3a4ab19d897ea7cd75a8d849cb729
Just161 <109623921+just1984@users.noreply.github.com>
2023-05-24 15:32:56 +0200

    Add files via upload

---
+++ grundlagen-tag-01-ide-just1984/src/main/kotlin/01_KompilierenUndAusfuehren.kt
@@ -0,0 +1,16 @@
+// Jedes Programm braucht eine Main Funktion
+fun main() {
+
+    // Liest den Namen ein, und begrüßt den Nutzer
+    println("Hallo, wie heißt du? (confirm with Enter)")
+    var name = readln()
+    println("Hallo $name!")
+
+    // Druckt eine leere Zeile aus
+    println()
+
+    // Liest das Alter ein
+    println("Wie alt bist du? (confirm with Enter)")
+    var alter = readln()    // Diese Zeile liest das Alter ein
+    println("Wow! Bereits $alter Jahre?!")
+}
---
+++ grundlagen-tag-01-ide-just1984/src/main/kotlin/02_FachbegriffeVerstehen.kt
@@ -0,0 +1,53 @@
+/* 
+Du hast heute verschiedene Fachbegriffe kennengelernt. Sieh dir zur Wiederholung die folgenden Aussagen an und überlege, welche wahr sind und welche nicht. Bei jeder
+
+Aufgabe gibt es mindestens eine wahre Aussage, es sind aber auch mehrere wahre Aussagen möglich. Kreuze die wahren Aussagen an.
+(Setze ein x in die richtigen [ ] Klammern ) 
+
+**Der Code...**
+- [X] besteht aus Anweisungen
+- [ ] soll später auf einem Gerät ausgeführt werden.
+- [ ] besteht ausschließlich aus Zahlen.
+
+**Der Programmfluss...**
+- [ ] zeigt den Stromverbrauch deines Programms.
+- [ ] läuft normalerweise von unten nach oben ab.
+- [X] beschreibt die Reihenfolge, in der die Anweisungen eines Programms ausgeführt werden.
+
+**Die IDE...**
+- [ ] ist ein Programm, welches beim Erstellen von Programmen hilft.
+- [ ] steht für Identical Durability Exception.
+- [X] ist eine Programmierumgebung.
+
+**Syntax...**
+- [X] ist die genaue Schreibweise einer Anwendung.
+- [X] ist wichtig für die Lauffähigkeit eines Programms.
+- [X] ist in jeder Programmiersprache verschieden.
+
+**Kompilieren...**
+- [ ] kontrolliert, ob deine Texte Rechtschreibfehler enthalten.
+- [X] kontrolliert, ob die Syntax eines Programms stimmt.
+- [X] wird bei einem Problem durch einen Kompilierfehler unterbrochen.
+
+**Ausführen...**
+- [ ] lässt sich ein Programm nie.
+- [X] ist der Prozess, in dem das Programm auf einem Gerät gestartet wird.
+- [ ] heißt, das Programm zu beenden.
+
+
+-------------------------------
+
+Platz um deine Antwort für die Codevorhersage zu notieren: 
+
+println("Hello World!")
+println("Heute ist ein schöner Tag!")
+println("Sehr gut! Ihr habt es fast geschafft. \n Jetzt könnt ihr schon eure ersten Zeilen Code lesen.")
+
+Die Ausgabe wird so aussehen:
+
+Hello World!
+Heute ist ein schöner Tag!
+Sehr gut! Ihr habt es fast geschafft.
+ Jetzt könnt ihr schon eure ersten Zeilen Code lesen.
+
+*/ 
